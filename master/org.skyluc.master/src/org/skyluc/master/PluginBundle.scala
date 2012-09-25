5885ec4b10cf0302c7d759edac06461e981f86c2
Luc Bourlier <luc.bourlier@typesafe.com>
2012-09-25 17:19:16 +0200

    Created the master plugin

---
+++ master/org.skyluc.master/src/org/skyluc/master/PluginBundle.scala
@@ -0,0 +1,25 @@
+package org.skyluc.master
+
+import org.eclipse.ui.plugin.AbstractUIPlugin
+import org.osgi.framework.BundleContext
+
+object PluginBundle {
+
+  private var _default: PluginBundle = _
+
+  def default = _default
+
+}
+
+class PluginBundle extends AbstractUIPlugin {
+
+  override def start(context: BundleContext) {
+    super.start(context)
+    PluginBundle._default = this
+  }
+  
+  def id = getBundle().getSymbolicName()
+  
+  def version = getBundle().getVersion().toString()
+
+}
